c5aee98d562842c0a8e35d63a04402c53a0e1ff3
laura.cabezas <lauracs1213@gmail.com>
2022-10-27 21:52:31 -0500

    Student and Teacher classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+       // System.out.print("Enter date of birth in YYYY-MM-DD format: ");
+        Student student = new Student("Juana", "2000-02-26");
+        System.out.println(student.getAge());
+        System.out.println(student.getId());
+        System.out.println(student.getName());
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,36 @@
-import java.util.Date;
-
+import java.time.LocalDate;
+import java.time.Period;
 public class Student {
     private String name;
     private int id;
-    private Date dateOfBirth;
+    private static int count = 10000;
+    private String dateOfBirth;
+    private int age;
+
+    public Student (String name, String dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.age = ageCalculator(dateOfBirth);
+        this.id = count;
+        count++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int ageCalculator(String dateOfBirth) {
+        LocalDate dob = LocalDate.parse(dateOfBirth);
+        LocalDate curDate = LocalDate.now();
+        this.age = Period.between(dob, curDate).getYears();
+        return this.age;
+    }
 }
